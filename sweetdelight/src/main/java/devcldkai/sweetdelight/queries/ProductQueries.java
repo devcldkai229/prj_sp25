1cd1648944346c3e793d2e159c8f11b0baac420a
kaicoursces <kag40222@gmail.com>
2025-01-15 21:53:52 +0700

    Delete sweetdelight directory

--- sweetdelight/src/main/java/devcldkai/sweetdelight/queries/ProductQueries.java
+++
@@ -1,8 +0,0 @@
-package devcldkai.sweetdelight.queries;
-
-public class ProductQueries {
-
-    public static final String SELECT_ALL_PRODUCTS = "SELECT * FROM Products";
-
-    private ProductQueries(){}
-}
